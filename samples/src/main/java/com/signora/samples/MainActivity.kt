b99b67004ab64cb8ba428f983dca24eaed960d5b
深海里有坏龙龙 <seele.peng@gmail.com>
2023-04-02 08:50:08 +0300

    👷 Add github action workflow and assistance support

---
+++ samples/src/main/java/com/signora/samples/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.signora.samples
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.signora.samples.ui.Calendar
+import com.signora.samples.ui.theme.CalendarioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalendarioTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                    Calendar()
+                }
+            }
+        }
+    }
+}
---
+++ samples/src/main/java/com/signora/samples/ui/Calendar.kt
@@ -0,0 +1,39 @@
+package com.signora.samples.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.signora.calendario.ExpandableCalendar
+import com.signora.calendario.ui.theme.CalendarTheme
+import java.time.LocalDate
+
+@Composable
+fun Calendar() {
+    var currentDate by remember { mutableStateOf(LocalDate.now()) }
+
+    CalendarTheme {
+        Column {
+            ExpandableCalendar(
+                onDateSelect = {
+                    currentDate = it
+                }
+            )
+            Text(
+                text = "Selected date: $currentDate",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CalendarPreview() {
+    Calendar()
+}
